40bf8ba00f6c0c514a69d7d6408579554c503f39
PhilipMoeller <63282605+Taela1@users.noreply.github.com>
2022-06-29 14:27:52 +0200

    Update userdata.sh

--- userdata/userdata.sh
+++ userdata/userdata.sh
@@ -391,10 +391,10 @@ cat > /etc/3cxpbx/setupconfig.xml <<EOF
 </SetupConfig>
 EOF
 
+apt -y install nginx net-tools dphys-swapfile gnupg2
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
 apt update
-apt -y install nginx net-tools dphys-swapfile gnupg2
 rm -f /etc/nginx/sites-enabled/default
 systemctl reload nginx
 apt -y install 3cxpbx
